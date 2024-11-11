db8b86f434ef6fa55884a32df824e70f1d0a1676
Bthcorn <bowornthat.maize@gmail.com>
2024-11-12 02:46:52 +0700

    feat: create course page and course card

---
+++ src/app/types/course.type.ts
@@ -0,0 +1,13 @@
+export type CourseType = {
+  id: string;
+  title: string;
+  description: string;
+  thumbnail: string;
+  teacher: string;
+  category: string;
+  duration: number;
+  level: string;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+};
