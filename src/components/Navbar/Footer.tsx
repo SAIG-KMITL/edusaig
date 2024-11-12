3edc4596173bd346228bae6d8c1bc81efe3a5092
patdanai767 <pattproc@gmail.com>
2024-11-12 22:34:56 +0700

    feat: navbar & footer components

--- src/components/Navbar/Footer.tsx
+++ src/components/Navbar/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 
 function Footer() {
     return (
@@ -9,16 +10,48 @@ function Footer() {
             <div className="mt-6 flex gap-14">
                 <div>
                     <h1 className="text-lg font-bold uppercase">follow us</h1>
-                    <div className="grid md:grid-cols-2 gap-y-4 gap-x-8 text-gray-500 mt-3 font-bold">
-                        <Link href={"https://github.com/SAIG-KMITL"} className="hover:underline">Github</Link>
-                        <Link href={"https://www.instagram.com/saig.kmitl/"} className="hover:underline">Instagram</Link>
-                        <Link href={"https://web.facebook.com/saigkmitl"} className="hover:underline">Facebook</Link>
-                        <Link href={"https://www.tiktok.com/@kmitlofficial?_t=8rIZd9dlVMW&_r=1"} className="hover:underline">Tiktok</Link>
+                    <div className="grid md:grid-cols-2 gap-y-4 gap-x-8 text-gray-500 mt-3 font-bold text-sm">
+                        <Link href={"https://github.com/SAIG-KMITL"} className="hover:underline flex items-center">
+                            <Image
+                                src="/icons/github.svg"
+                                width={20}
+                                height={20}
+                                alt="github icon"
+                                className="mr-1"
+                            />
+                            Github</Link>
+                        <Link href={"https://www.instagram.com/saig.kmitl/"} className="hover:underline flex items-center">
+                            <Image
+                                src="/icons/instagram.svg"
+                                width={20}
+                                height={20}
+                                alt="ig icon"
+                                className="mr-1"
+                            />
+                            Instagram</Link>
+                        <Link href={"https://web.facebook.com/saigkmitl"} className="hover:underline flex items-center">
+                            <Image
+                                src="/icons/facebook.svg"
+                                width={20}
+                                height={20}
+                                alt="facebook icon"
+                                className="mr-1"
+                            />
+                            Facebook</Link>
+                        <Link href={"https://www.tiktok.com/@kmitlofficial?_t=8rIZd9dlVMW&_r=1"} className="hover:underline flex items-center">
+                            <Image
+                                src="/icons/tiktok.svg"
+                                width={20}
+                                height={20}
+                                alt="tiktok icon"
+                                className="mr-1"
+                            />
+                            Tiktok</Link>
                     </div>
                 </div>
                 <div>
                     <h1 className="text-lg font-bold uppercase">Resources for</h1>
-                    <div className="grid md:grid-cols-2 gap-y-4 gap-x-8 text-gray-500 mt-3 font-bold">
+                    <div className="grid md:grid-cols-2 gap-y-4 gap-x-8 text-gray-500 mt-3 font-bold text-sm">
                         <Link href={""} className="hover:underline">Careers</Link>
                         <Link href={""} className="hover:underline">Students</Link>
                         <Link href={""} className="hover:underline">Investors</Link>
@@ -27,7 +60,7 @@ function Footer() {
                 </div>
                 <div>
                     <h1 className="text-lg font-bold uppercase">legal</h1>
-                    <div className="grid gap-y-4 gap-x-8 lg:mr-[100px] text-gray-500 mt-3 font-bold">
+                    <div className="grid gap-y-4 gap-x-8 lg:mr-[100px] text-gray-500 mt-3 font-bold text-sm">
                         <Link href={""} className="hover:underline">Privacy Policy</Link>
                         <Link href={""} className="hover:underline">Terms & Conditions</Link>
                     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                 <Link href={"/home"} className='hover:text-sky-500 transition py-3 px-1'>Home</Link>
                 <Link href={"/course"} className='hover:text-sky-500 transition py-3 px-1'>Course</Link>
                 <Link href={"/roadmap"} className='hover:text-sky-500 transition py-3 px-1'>Roadmap</Link>
-                <Link href={"/rewards"} className='hover:text-sky-500 transition py-3 px-1'>Rewards</Link>
+                <Link href={"/reward"} className='hover:text-sky-500 transition py-3 px-1'>Rewards</Link>
             </div>
                 <Link href={"/login"} className='border px-5 py-2 bg-trans-white rounded-full border-skyblue transition mr-5 background bg-sky-500 font-semibold text-white hover:bg-sky-700'>Get Started</Link>
         </div>
