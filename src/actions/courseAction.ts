4cee11a8cd30c86be9df99c3752de93ea901601d
patdanai767 <pattproc@gmail.com>
2024-11-26 00:02:27 +0700

    feat: integrate course page with api action

--- src/actions/courseAction.ts
+++ src/actions/courseAction.ts
@@ -15,3 +15,17 @@ export async function fetchCourseAction(id: string) {
     requiresAuth: true,
   });
 }
+
+export async function fetchCourseMostEnrollAction(){
+  return baseApiAction<CoursesResponseType>(`/course/most-enroll?page=1&limit=10`,{
+    method: "GET",
+    requiresAuth:true
+  })
+}
+
+export async function fetchCourseNewArrivalsAction(){
+  return baseApiAction<CoursesResponseType>(`/course/new-arrivals?page=1&limit=10`,{
+    method: "GET",
+    requiresAuth:true
+  })
+}
