906962d6867d8ef415f500395b73a16c860a6842
Jetsarit Kaewsaart <applerew14112546@gmail.com>
2024-11-25 12:08:03 +0700

    feat: create, edit, delete course module integration

--- src/app/(dashboard)/dashboard/course/[id]/course-module/page.tsx
+++ src/app/(dashboard)/dashboard/course/[id]/course-module/page.tsx
@@ -1,5 +1,33 @@
+import { fetchChaptersAction } from "@/actions/chapterAction";
+import { fetchCourseAction } from "@/actions/courseAction";
+import { fetchCourseModulesByCourseAction } from "@/actions/courseModuleAction";
 import CourseModuleDashboardUI from "@/app/shared/(ui)/CourseModuleDashboardUI";
 
-export default function CourseModulePage() {
-  return <CourseModuleDashboardUI />
+interface CourseModulePageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function CourseModulePage({ params }: CourseModulePageProps) {
+  const { id } = await params;
+
+  const courseResponse = await fetchCourseAction(id);
+  if (!courseResponse.data) {
+    return null;
+  }
+
+  const courseModulesResponse = await fetchCourseModulesByCourseAction(id);
+  if (!courseModulesResponse.data) {
+    return null;
+  }
+
+  const chaptersResponse = await fetchChaptersAction();
+  if(!chaptersResponse.data?.data) {
+    return null;
+  }
+
+  return <CourseModuleDashboardUI 
+    course={courseResponse.data} 
+    courseModules={courseModulesResponse.data}
+    chapters={chaptersResponse.data.data}
+  />
 }
