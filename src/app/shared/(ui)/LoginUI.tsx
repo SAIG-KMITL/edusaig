bde2eb5a9f4a5ae5a4ddc91e7ae835122b6af006
Proxiermax <teethawat9438@gmail.com>
2024-11-28 21:39:17 +0700

    feat: add layout component and enhance login UI with background effects

--- src/app/shared/(ui)/LoginUI.tsx
+++ src/app/shared/(ui)/LoginUI.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { loginAction } from "@/actions/authAction";
 import Input from "@/components/Inputs/Input";
 import { Toast } from "@/components/Toast/Toast";
@@ -6,12 +7,14 @@ import { loginSchema } from "@/schema/login.schema";
 import { SERVICE_AUTH } from "@/utils/enums/service-auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Cookies from "js-cookie";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import authEffect from "@/../public/ulits/auth-effect.svg";
 
 type LoginFormData = z.infer<typeof loginSchema>;
 
@@ -55,59 +58,88 @@ const LoginUI = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
-        <div className="max-w-md text-center">
+    <div className="relative flex justify-center items-center h-screen">
+      <motion.div
+        whileHover={{ scale: 1.02 }}
+        className="absolute top-4 left-4 font-bold text-[26px] leading-[100%] text-white flex ml-5 cursor-pointer"
+      >
+        <Link href="/" className="flex flex-row items-center gap-3">
           <Image
-            src="/icons/banner.svg"
-            alt="login"
-            width={500}
-            height={500}
-            className="object-cover"
+            src="/icons/logo.svg"
+            width={32}
+            height={32}
+            alt="logo"
+            className="pb-2"
           />
-        </div>
-      </div>
-      <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
-        <div className="max-w-md w-full p-6">
-          <h1 className="text-3xl font-semibold mb-6 text-black text-center">
-            Sign Up
-          </h1>
-          <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">
-            Join to Our Community with all time access and free
-          </h1>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <Input
-              type="email"
-              label="Email"
-              placeholder="Email"
-              error={errors.email}
-              {...register("email")}
+          <p className="flex flex-row items-baseline">
+            EDUSA
+            <Image
+              src="/icons/wand.svg"
+              width={24}
+              height={24}
+              alt="wand icon"
             />
-            <Input
-              type="password"
-              label="Password"
-              placeholder="Password"
-              error={errors.password}
-              {...register("password")}
-            />
-            <div>
-              <button
-                type="submit"
-                disabled={isLoading}
-                className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
-              >
-                {isLoading ? "Loading..." : "Login"}
-              </button>
-            </div>
-          </form>
-          <div className="mt-4 text-sm text-gray-600 text-center">
-            <p>
-              Doesn&apos;t have an account?{" "}
-              <Link href="/register" className="text-black hover:underline">
-                Sign In
-              </Link>
-            </p>
+            G
+          </p>
+        </Link>
+      </motion.div>
+
+      <Image
+        src={authEffect}
+        alt="auth effect"
+        width={500}
+        height={500}
+        className="absolute top-4 left-16 object-cover">
+      </Image>
+
+      <Image
+        src={authEffect}
+        alt="auth effect"
+        width={500}
+        height={500}
+        className="absolute bottom-4 right-16 object-cover">
+      </Image>
+
+      <div className="flex flex-col space-y-4 w-full max-w-[720px] min-w-[360px] mx-36 px-16 md:px-32 py-4 rounded-3xl bg-purple-100 bg-opacity-10 backdrop-blur-md">
+        <h1 className="text-[36px] md:text-[72px] font-semibold text-white text-center">
+          LOGIN
+        </h1>
+        <form action="" onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-4">
+          <Input
+            type="email"
+            label="Email"
+            placeholder="Email"
+            error={errors.email}
+            {...register("email")}
+            labelClassName="text-white"
+          />
+          <Input
+            type="password"
+            label="Password"
+            placeholder="Password"
+            error={errors.password}
+            {...register("password")}
+            labelClassName="text-white"
+          />
+          <div>
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="w-full bg-gradient-to-r from-indigo-500 from-0% via-purple-500 via-50% to-sky-500 to-100% text-xl font-semibold text-white p-2 rounded-xl focus:outline-none focus:bg-white focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 transition-colors duration-300"
+            >
+              {isLoading ? "Loading..." : "LOGIN"}
+            </button>
           </div>
+        </form>
+        <hr />
+        <div className="m-4 text-white text-center">
+          <p>
+            Not a member?{" "}
+            <Link href="/register" className="text-sky-300 hover:underline">
+              Sign up now
+            </Link>
+          </p>
         </div>
       </div>
     </div>
